2d226286d0574ec67f80b31d36e741a5285a4fc1
Alireza <are3002@outlook.com>
2024-04-24 09:32:51 +0330

    Add models for Cash app

--- cash/models.py
+++ cash/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Cost(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    amount = models.IntegerField()
+    date = models.DateField(defult = auto_now_add)
+    category = models.ForeignKey('Category')
+
+class Income(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    amount = models.IntegerField()
+    date = models.DateField(defult = auto_now_add)
+    source = models.ForeignKey('Source')
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Source(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
