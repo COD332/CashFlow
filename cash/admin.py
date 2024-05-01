53a3b593de4ccdbcc323c5208e264ea6ea15b76e
Alireza <are3002@outlook.com>
2024-05-01 11:50:43 +0330

    Update time zone in settings.py and add migrations for altering cost date field

--- cash/admin.py
+++ cash/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Cost, Income, Category, Source
 
-admin.site.register(Cost)
-admin.site.register(Income)
-admin.site.register(Category)
-admin.site.register(Source)
+class CostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount', 'date', 'category']
+    list_filter = ['date', 'category']
+    search_fields = ['title', 'description']
+    readonly_fields = ['date']
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount', 'date', 'source']
+    list_filter = ['date', 'source']
+    search_fields = ['title', 'description']
+    readonly_fields = ['date']
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+admin.site.register(Cost, CostAdmin)
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Source, SourceAdmin)
--- cash/models.py
+++ cash/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 
 class Cost(models.Model):
+    class Meta :
+        ordering = ['-date']
+    
     def __str__(self):
         return self.title
     
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     amount = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
 class Income(models.Model):
+    class Meta :
+        ordering = ['-date']
+    
     def __str__(self):
         return self.title
     
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     amount = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     source = models.ForeignKey('Source', on_delete=models.PROTECT)
 
 class Category(models.Model):
